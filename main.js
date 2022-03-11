608203207a2dc390dd6c6240311a4b8a6aaf96c3
justinkim2015 <justinkim2015@gmail.com>
2022-03-12 08:46:44 +0900

    Edit conditionals_stillbroken

--- main.js
+++ main.js
@@ -65,28 +65,32 @@ function playRound(playerSelection, computerSelection) {
 function game() {
 
     let myscore = 0;
-    let compscore= 0;    
+    let compscore = 0;    
+    let tie = 0
 
     for (let i=0; i < 5; i++) {
       playerSelection = prompt('Rock, paper, scissors?');
       computerSelection = computerPlay();
       console.log(playRound(playerSelection, computerSelection))
-      if (playRound(playerSelection, computerSelection) === 
-         'You lose! Paper beats rock!' || 
-         'You lose! Scissors beats paper!' || 
-         'You lose! Rock beats scissors!') {
-          //compscore += 1;
-      } else if (playRound(playerSelection, computerSelection) ===  
-         'You win! Rock beats Paper!' || 
-         'You win! Paper beats rock!' || 
-         'You win! Scissors beats paper!') {
+
+
+      if (playRound(playerSelection, computerSelection) == 'You lose! Paper beats rock!'  
+         || (playRound(playerSelection, computerSelection) == 'You lose! Scissors beats paper!'  
+         || (playRound(playerSelection, computerSelection) == 'You lose! Rock beats scissors!'))) {
+          compscore += 1;
+      } else if (playRound(playerSelection, computerSelection) ==  'You win! Rock beats Paper!' 
+         || (playRound(playerSelection, computerSelection) == 'You win! Paper beats rock!' 
+         || (playRound(playerSelection, computerSelection) == 'You win! Scissors beats paper!'))) {
           myscore += 1;
       } else {
       }
-      console.log(myscore,compscore)
+
+      console.log(myscore, compscore)
       }
 }
 
+//MY CONDITIONALS ARE MISTAKEN*************
+//*******OR SYNTAX ERROR SOMEWHERE IN CONDITIONALS
 //Problem is compscore is incrementing by 1 no matter what.
 //comp score works but increments every round, if i comment it out, myscore =+ 1 doesnt  run.
 //perhaps its not reading my else if, or else, statement, time to check my syntax
