438b133ffa5cc9c3901224bcb7e8e8be29f6b53b
justinkim2015 <justinkim2015@gmail.com>
2022-03-16 20:34:35 +0900

    Add window.onload to use external JS

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-//pick a random number 0 to 2
-//If number is 0 return the value rock
-//if number is 1 return the value paper
-//if number is 2 return the value scissors
-
+window.onload = function () { //WHY DOES THIS WORK STUDYSTUDY
 
 function computerPlay() {
     let random = Math.floor(Math.random()*3);
@@ -14,16 +10,6 @@ function computerPlay() {
         return 'scissors';
     }
 }
-
-//declare a function
-//give two parameters playerselection and computerselection
-//create a way to have playerselection input into the game
-//make the computer selection parameter call the computerPlay function
-//write conditonal statements within the function to follow logic of RPS
-//make playerSelection case insensitive 
-//determine the winner of the match
-//return a string declaring the winner.
-
 
 function playRound(playerSelection, computerSelection) {
      computerSelection = computerPlay();
@@ -49,51 +35,46 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-//declare a function*
-//call the playRound function inside the function*
-//use prompt() to get input from the user*
-//console.log() the results of the match*
-//find a way to repeat this until there are 5 rounds* 
-//find a way to increment numbers*
-//find a way to keep score
-//return score at end and declare winner
-//myscore ++
-//compscore ++;
-//console.log(playRound(playerSelection, computerSelection));
-// console.log(myscore, compscore);
 
-function game() {
+// function game() {
 
-    let myscore = 0;
-    let compscore = 0;    
-    let tie = 0
+//     let myscore = 0;
+//     let compscore = 0;    
+//     let tie = 0
 
-    for (let i=0; i < 5; i++) { //start a loop
-      playerSelection = prompt('Rock, paper, scissors?'); //prompt the player and get playerSelection value
-      computerSelection = computerPlay(); //call the function computerPlay to get computerSelection value
-      if (playRound(playerSelection,computerSelection) == 'You win!') {
-          myscore++;
-          console.log('You win!')  
-      } else if (playRound(playerSelection, computerSelection) == 'You lose!') {
-          compscore++;
-          console.log('You lose');
-      } else {
-          console.log('It\'s a tie!');
-      }
-      console.log(myscore, compscore)
-      }
+//     for (let i=0; i < 5; i++) {
+//       playerSelection = prompt('Rock, paper, scissors?');
+//       computerSelection = computerPlay(); 
+//       if (playRound(playerSelection,computerSelection) == 'You win!') {
+//           myscore++;
+//           console.log('You win!')  
+//       } else if (playRound(playerSelection, computerSelection) == 'You lose!') {
+//           compscore++;
+//           console.log('You lose');
+//       } else {
+//           console.log('It\'s a tie!');
+//       }
+//       console.log(myscore, compscore)
+//       }
 
-      if (myscore > compscore) {
-          return `You win! ${myscore} to ${compscore}`; 
-      } else if (compscore > myscore) {
-          return `You lose! ${myscore} to ${compscore}`;
-      } else {
-          return `It's a tie!  ${myscore} to ${compscore}`;
-      }
+//       if (myscore > compscore) {
+//           return `You win! ${myscore} to ${compscore}`; 
+//       } else if (compscore > myscore) {
+//           return `You lose! ${myscore} to ${compscore}`;
+//       } else {
+//           return `It's a tie!  ${myscore} to ${compscore}`;
+//       }
+// }
+
+const button = document.querySelector('#rock')
+
+button.addEventListener('click', playRound) 
+
+
+
+
+
+
+
+
 }
-
-//The big problem I had with this was WHEN i was console.log() -ing playRound().  
-//I was actually calling the function two times with different results, therefore the scoring 
-//was behaving unpredictably.  
-
-//I fixed this by simplifying the code and not pulling the returned value from the function but just a string I wrote.
